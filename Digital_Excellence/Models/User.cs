80d60ddb8f3a35e139eef068b8f32548a8017271
Rene <r.vanvliet@student.fontys.nl>
2020-03-04 17:10:48 +0100

    created Initial CRUD backend

---
+++ Digital_Excellence/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+
+		public User()
+		{
+		}
+
+		public User(int userId) : this()
+		{
+			Id = userId;
+		}
+		public string Username { get; set; }
+		public string ProfileUrl { get; set; }
+
+	}
+
+}
