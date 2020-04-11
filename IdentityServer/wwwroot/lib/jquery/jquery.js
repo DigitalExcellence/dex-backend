c18dbcd206bf045ac907a1d8b68280452e714705
Brend Smits <brend_smits@hotmail.com>
2020-04-11 16:19:25 +0200

    Added GNU Lesser General Public license version 3 to the solution.

--- IdentityServer/wwwroot/lib/jquery/jquery.js
+++ IdentityServer/wwwroot/lib/jquery/jquery.js
@@ -1,17 +1,3 @@
-/*!
- * jQuery JavaScript Library v2.1.4
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2015-04-28T16:01Z
- */
-
 (function( global, factory ) {
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
