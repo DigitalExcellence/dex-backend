2e57f14af5e2853f1a0d1d764763486bed4819fa
Joris Coenen <joris_coenen-@hotmail.com>
2020-05-04 13:59:20 +0200

    Added more commentary

--- 3_Repositories.Tests/DataGenerators/Base/FakeDataGenerator.cs
+++ 3_Repositories.Tests/DataGenerators/Base/FakeDataGenerator.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 
 namespace Repositories.Tests.DataGenerators.Base
 {
+    /// <summary>
+    /// Base class to inherit the fakeDataGenerators from
+    /// </summary>
+    /// <typeparam name="T">Domain class from the Models</typeparam>
     public class FakeDataGenerator<T> : IFakeDataGenerator<T> where T : class
     {
         protected Faker<T> Faker;
 
+        /// <summary>
+        /// Initializes fakeDataGenerator
+        /// </summary>
         public FakeDataGenerator()
         {
             Faker = new Faker<T>();
         }
 
+        ///<inheritdoc cref="IFakeDataGenerator{TDomain}"/>
         public T Generate()
         {
             return Faker.Generate();
         }
 
+        ///<inheritdoc cref="IFakeDataGenerator{TDomain}"/>
         public IEnumerable<T> GenerateRange(int amount)
         {
             return Faker.Generate(amount);
