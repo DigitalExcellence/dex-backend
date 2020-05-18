746d009abcec9b83c5f119d9f4989c23c44397a4
Ramon <r.peek@student.fontys.nl>
2020-05-18 11:38:29 +0200

    Updated comments at HighlightDataSourceAttribute

--- 3_Repositories.Tests/DataSources/HighlightDataSourceAttribute.cs
+++ 3_Repositories.Tests/DataSources/HighlightDataSourceAttribute.cs
@@ -10,22 +10,38 @@ using System.Text;
 
 namespace Repositories.Tests.DataSources
 {
+{
+    /// <summary>
+    /// Attribute to generate highlights
+    /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
     public class HighlightDataSourceAttribute : Attribute, IParameterDataSource
     {
         private readonly IFakeDataGenerator<Highlight> fakeDataGenerator;
         private readonly int amountToGenerate = 0;
 
+        /// <summary>
+        /// Initializes highlightDataSourceAttribute
+        /// </summary>
         public HighlightDataSourceAttribute()
         {
             fakeDataGenerator = new HighlightDataGenerator();
         }
 
+        /// <summary>
+        /// Initializes highlightDataSourceAttribute
+        /// and setting the amount of highlights to be generated
+        /// </summary>
         public HighlightDataSourceAttribute(int amount) : this()
         {
             amountToGenerate = amount;
         }
 
+        /// <summary>
+        /// Generate the data and return it
+        /// </summary>
+        /// <param name="parameter">Extra parameters given in the attribute, not in use but required due to inheritance</param>
+        /// <returns>The generated data</returns>
         public IEnumerable GetData(IParameterInfo parameter)
         {
             if(amountToGenerate <= 1)
