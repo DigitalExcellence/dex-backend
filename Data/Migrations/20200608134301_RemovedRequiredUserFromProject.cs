5217eee06126652c87cc1bdd95010070d664c9fb
Ramon Peek <ramonpeek@hotmail.com>
2020-06-08 15:50:10 +0200

    Added empty migration

---
+++ Data/Migrations/20200608134301_RemovedRequiredUserFromProject.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _4_Data.Migrations
+{
+    public partial class RemovedRequiredUserFromProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //TODO CHANGE MIGRATIONDATA
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //TODO CHANGE MIGRATIONDATA
+        }
+    }
+}
