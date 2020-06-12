8b6441edc6fad45e7ced4d95bb1ba9e4333380a0
RensvdLinden <rjt.vanderlinden@student.fontys.nl>
2020-06-12 12:33:29 +0200

    Removed unused imports
    Removed unused imports

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -16,9 +16,7 @@
 */
 
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Sentry;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
