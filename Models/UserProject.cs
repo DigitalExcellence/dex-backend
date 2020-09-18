c405ff09ae457516b722c48645045d4990a917d1
Mees van Straten <41986758+MeesvanStraten@users.noreply.github.com>
2020-09-18 14:25:15 +0200

    Created linking UserProject table
    changed user model tho have UserProject object.

---
+++ Models/UserProject.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+
+    public class UserProject
+    {
+
+        public int IdentityId { get; set; }
+        public User User { get; set; }
+        public int Id { get; set; }
+        public Project Project { get; set; }
+
+    }
+
+}
