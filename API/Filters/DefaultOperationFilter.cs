f540ad81ab260d71524bedb052b46bc2dc975d0f
Ruben <rubenfricke@gmail.com>
2020-09-21 10:20:57 +0200

    Won't override endpoints with consume attribute

--- API/Filters/DefaultOperationFilter.cs
+++ API/Filters/DefaultOperationFilter.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
 
 namespace API.Filters
 {
@@ -17,6 +19,17 @@ namespace API.Filters
         /// <param name="context">Filter context</param>
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
+            bool hasConsumeAttribute = context.MethodInfo.GetCustomAttributes(true)
+                                              .Union(context.MethodInfo.GetCustomAttributes(true))
+                                              .OfType<ConsumesAttribute>()
+                                              .Count() != 0;
+            if(operation.RequestBody != null && !hasConsumeAttribute)
+            {
+                var mediaType = operation.RequestBody.Content["application/json"];
+                operation.RequestBody.Content.Clear();
+                operation.RequestBody.Content.Add("application/json", mediaType);
+            }
+
             foreach(string code in operation.Responses.Keys)
             {
                 if(operation.Responses[code]
