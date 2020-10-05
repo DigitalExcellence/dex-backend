1fc9495fbdcd0602191f16ed936f25d605b4b684
aintabb <emreozvarlik@gmail.com>
2020-10-04 21:40:29 -0700

    Add missing migration files for "LikedProjectByUser" table.

---
+++ Data/Migrations/20201005043814_InitLikedProjectByUserTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _4_Data.Migrations
+{
+    public partial class InitLikedProjectByUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LikedProjectsByUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LikedProjectId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikedProjectsByUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LikedProjectsByUsers_Project_LikedProjectId",
+                        column: x => x.LikedProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LikedProjectsByUsers_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikedProjectsByUsers_LikedProjectId",
+                table: "LikedProjectsByUsers",
+                column: "LikedProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikedProjectsByUsers_UserId",
+                table: "LikedProjectsByUsers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LikedProjectsByUsers");
+        }
+    }
+}
