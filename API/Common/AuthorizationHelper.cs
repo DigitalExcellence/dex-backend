e53c9a5bd171fe37e39f3facc100c18c008e2a99
Ruben <rubenfricke@gmail.com>
2020-10-29 11:49:17 +0100

    Added documentation for the authorization helper

--- API/Common/AuthorizationHelper.cs
+++ API/Common/AuthorizationHelper.cs
@@ -1,3 +1,20 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You can find a copy of the GNU Lesser General Public License
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 using Models;
 using Services.Services;
 using System.Threading.Tasks;
@@ -5,7 +22,7 @@ using System.Threading.Tasks;
 namespace API.Common
 {
     /// <summary>
-    /// 
+    /// The implementation for the authorization helper.
     /// </summary>
     public class AuthorizationHelper : IAuthorizationHelper
     {
@@ -13,22 +30,26 @@ namespace API.Common
         private readonly IUserService userService;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="AuthorizationHelper"/> class.
         /// </summary>
-        /// <param name="userService"></param>
+        /// <param name="userService">The user service for communicating with the logic layer.</param>
         public AuthorizationHelper(IUserService userService)
         {
             this.userService = userService;
         }
 
         /// <summary>
-        /// 
+        /// This method checks if a user has the correct scope to use the endpoint.
+        /// This method checks for a normal scope and the data officer scope within the
+        /// same institution.
         /// </summary>
-        /// <param name="loggedInUser"></param>
-        /// <param name="scope"></param>
-        /// <param name="dataOfficerScope"></param>
-        /// <param name="propertyOfUserId"></param>
-        /// <returns></returns>
+        /// <param name="loggedInUser">The user model of the logged in user.</param>
+        /// <param name="scope">The required scope for accessing this endpoint.</param>
+        /// <param name="dataOfficerScope">The required scope for accessing this
+        /// endpoint for data officers within the same institution.</param>
+        /// <param name="propertyOfUserId">The id of the user owner of the property
+        /// which the logged in user wants to access.</param>
+        /// <returns>bool: true if the user is allowed, false if the user is not allowed.</returns>
         public async Task<bool> UserIsAllowed(User loggedInUser, string scope, string dataOfficerScope, int propertyOfUserId)
         {
             bool hasUserWriteScope = userService.UserHasScope(loggedInUser.IdentityId, scope);
