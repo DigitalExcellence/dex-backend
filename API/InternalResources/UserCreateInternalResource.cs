88c54d500bbd885c88bc1a2a8c9c42c18f120e93
Ruben <rubenfricke@gmail.com>
2020-11-19 14:16:11 +0100

    Create UserCreateInternalResource.cs

---
+++ API/InternalResources/UserCreateInternalResource.cs
@@ -0,0 +1,17 @@
+namespace API.InternalResources
+{
+
+    public class UserCreateInternalResource
+    {
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string IdentityId { get; set; }
+
+        public string IdentityInsitutionId { get; set; }
+
+    }
+
+}
