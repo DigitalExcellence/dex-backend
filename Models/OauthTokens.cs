3f6e912a42e24203715f47b7fc881a67efda4c84
Ruben <rubenfricke@gmail.com>
2020-11-26 14:48:47 +0100

    Create OauthTokens model

---
+++ Models/OauthTokens.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+
+namespace Models
+{
+
+    public class OauthTokens
+    {
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public string ExpiresIn { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+    }
+
+}
