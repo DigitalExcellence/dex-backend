858ae21c3108e724e0ce17b050b03a88823a5dd6
Ruben <rubenfricke@gmail.com>
2020-11-26 15:23:12 +0100

    Implemented reflection for dynamic object creation

--- Services/DataProviders/DataProviderLoader.cs
+++ Services/DataProviders/DataProviderLoader.cs
@@ -1,5 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Models.DataProviders
 {
@@ -15,15 +20,35 @@ namespace Models.DataProviders
 
     public class DataProviderLoader : IDataProviderLoader
     {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+
+        public DataProviderLoader(IServiceScopeFactory serviceScopeFactory)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+        }
 
         public IEnumerable<IDataSource> GetAllDataSources()
         {
-            throw new NotImplementedException();
+            List<IDataSource> dataSources = new List<IDataSource>();
+            using IServiceScope scope = serviceScopeFactory.CreateScope();
+            foreach(string dll in Directory.GetFiles(Assembly.GetEntryAssembly().Location, "*.dll"))
+            {
+                Assembly assembly = Assembly.LoadFrom(dll);
+                foreach(Type type in assembly.GetTypes())
+                {
+                    if(type.GetInterface("IDataSource") != typeof(IDataSource)) continue;
+                    IDataSource dataSource = scope.ServiceProvider.GetService(type) as IDataSource;
+                    dataSources.Add(dataSource);
+                }
+            }
+
+            return dataSources;
         }
 
         public IDataSource GetDataSourceByGuid(string guid)
         {
-            throw new NotImplementedException();
+            return GetAllDataSources()
+                .SingleOrDefault(d => d.Guid == guid);
         }
 
     }
