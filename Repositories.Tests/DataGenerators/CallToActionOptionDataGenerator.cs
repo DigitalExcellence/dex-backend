14ee576cd68b597834a92da13df9458de29c08f6
Ruben <rubenfricke@gmail.com>
2020-11-27 13:27:19 +0100

    Added data source gens

---
+++ Repositories.Tests/DataGenerators/CallToActionOptionDataGenerator.cs
@@ -0,0 +1,44 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You can find a copy of the GNU Lesser General Public License
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
+using Bogus;
+using Models;
+using Repositories.Tests.DataGenerators.Base;
+
+namespace Repositories.Tests.DataGenerators
+{
+
+    public class CallToActionOptionDataGenerator : FakeDataGenerator<CallToActionOption>
+    {
+
+        public CallToActionOptionDataGenerator()
+        {
+            Faker = new Faker<CallToActionOption>()
+                    .RuleFor(option => option.Id, faker => faker.IndexFaker)
+                    .RuleFor(option => option.Value, faker => faker.Name.FirstName())
+                    .RuleForType(typeof(CallToActionOptionType),
+                                 faker => new CallToActionOptionType
+                                          {
+                                              Id = faker.IndexFaker,
+                                              Name = faker.Name.FirstName(),
+                                              Description = faker.Lorem.Paragraph()
+                                          });
+        }
+
+    }
+
+}
