923228d0fffdb4f11f68ec830ea7590eef68ff2d
Max van Hattum <438466@student.fontys.nl>
2020-12-15 12:47:42 +0100

    Added xml documentation to MessageBroker Publisher

--- MessageBrokerPublisher/Services/RabbitMQConnectionFactory.cs
+++ MessageBrokerPublisher/Services/RabbitMQConnectionFactory.cs
@@ -5,19 +5,32 @@ using System.Text;
 
 namespace MessageBrokerPublisher.Services
 {
+    ///<summary>
+    ///Interface for the RabbitMQConnectionFactory.
+    ///</summary>
+    ///
+
     public interface IRabbitMQConnectionFactory
     {
+        ///<summary>
+        /// Method returns a connection
+        /// </summary>
         IConnection CreateConnection();
 
     }
 
+    /// <summary>
+    /// This class is responsible for creating and return a connection to the messagebroker.
+    /// </summary>
     public class RabbitMQConnectionFactory : IRabbitMQConnectionFactory
     {
         private readonly string hostName = Environment.GetEnvironmentVariable("App__RabbitMQ__Hostname");
         private readonly string user = Environment.GetEnvironmentVariable("App__RabbitMQ__Username");
         private readonly string password = Environment.GetEnvironmentVariable("App__RabbitMQ__Password");
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RabbitMQConnectionFactory"/> class.
+        /// </summary>
         public RabbitMQConnectionFactory()
         {
             if(string.IsNullOrEmpty(hostName))
@@ -29,6 +42,9 @@ namespace MessageBrokerPublisher.Services
         }
 
 
+        /// <summary>
+        /// Creates a new RabbitMQ Connection Factory
+        /// </summary>
         private ConnectionFactory CreateConnectionFactory()
         {
             return new ConnectionFactory
@@ -38,6 +54,9 @@ namespace MessageBrokerPublisher.Services
                 HostName = hostName
             };
         }
+        /// <summary>
+        /// Creates a new RabbitMQ Connection and returns it.
+        /// </summary>
         public IConnection CreateConnection()
         {
             ConnectionFactory connectionFactory = CreateConnectionFactory();
