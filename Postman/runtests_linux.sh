fe80351691d59ea8980e87a7e4936c2719f54f24
Niray <niray11@live.nl>
2021-01-06 10:03:38 +0100

    Changed newman files so test will run 3 iterations
    This will provide us a better average performance time.

--- Postman/runtests_linux.sh
+++ Postman/runtests_linux.sh
@@ -1,5 +1,5 @@
 cd ..
 
-newman run Postman/dex.postman_collection.json -e Postman/local.postman_environment.json -k --silent
+newman run Postman/dex.postman_collection.json -e Postman/local.postman_environment.json -k --silent -n 3
 
 $SHELL
--- Postman/runtests_windows.bat
+++ Postman/runtests_windows.bat
@@ -1,2 +1,2 @@
 cd ..
-start cmd /k newman run Postman/dex.postman_collection.json -e Postman/local.postman_environment.json -k
+start cmd /k newman run Postman/dex.postman_collection.json -e Postman/local.postman_environment.json -k -n 3
