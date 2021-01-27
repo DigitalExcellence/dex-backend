72074e5ac52fc56035a822799ad80fa1870b7680
Ruby77 <rubenfricke@gmail.com>
2021-01-27 14:29:29 +0100

    Added swagger description for Data Source controller

--- API/Controllers/DataSourceController.cs
+++ API/Controllers/DataSourceController.cs
@@ -1,3 +1,20 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You can find a copy of the GNU Lesser General Public License
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 using API.HelperClasses;
 using API.Resources;
 using AutoMapper;
@@ -15,7 +32,7 @@ using System.Threading.Tasks;
 namespace API.Controllers
 {
     /// <summary>
-    ///
+    /// This class is responsible for handling HTTP requests that are related to the data sources, for example retrieving and updating.
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
