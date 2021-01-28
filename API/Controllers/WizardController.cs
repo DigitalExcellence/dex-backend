b04609b1fee26f2dbee866140f153138014bbb05
Ruby77 <rubenfricke@gmail.com>
2021-01-28 16:05:58 +0100

    Catched possible External Exceptions in controller + some general improvements

--- API/Controllers/WizardController.cs
+++ API/Controllers/WizardController.cs
@@ -25,10 +25,12 @@ using Models.Exceptions;
 using Services.ExternalDataProviders;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
+
     /// <summary>
     /// This class is responsible for handling HTTP requests that are related
     /// to the wizard, for exampling retrieving.
@@ -37,6 +39,7 @@ namespace API.Controllers
     [ApiController]
     public class WizardController : ControllerBase
     {
+
         private readonly IDataProviderService dataProviderService;
         private readonly IMapper mapper;
 
@@ -77,33 +80,33 @@ namespace API.Controllers
             if(sourceUri == null)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Source uri is null or empty.",
-                    Detail = "The incoming source uri is not valid.",
-                    Instance = "6D63D9FA-91D6-42D5-9ACB-461FBEB0D2ED"
-                };
+                                         {
+                                             Title = "Source uri is null or empty.",
+                                             Detail = "The incoming source uri is not valid.",
+                                             Instance = "6D63D9FA-91D6-42D5-9ACB-461FBEB0D2ED"
+                                         };
                 return BadRequest(problem);
             }
 
             if(!Guid.TryParse(dataSourceGuid, out Guid _))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Specified guid is not valid.",
-                    Detail = "The specified guid is not a real or valid guid.",
-                    Instance = "9FAF4C56-5B09-46C2-9A52-902D82ADAFA6"
-                };
+                                         {
+                                             Title = "Specified guid is not valid.",
+                                             Detail = "The specified guid is not a real or valid guid.",
+                                             Instance = "9FAF4C56-5B09-46C2-9A52-902D82ADAFA6"
+                                         };
                 return BadRequest(problem);
             }
 
             if(!dataProviderService.IsExistingDataSourceGuid(dataSourceGuid))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Data source not found.",
-                    Detail = "Data source could not be found with specified data source guid.",
-                    Instance = "DA33EB64-13EF-46CC-B3E6-785E4027377A"
-                };
+                                         {
+                                             Title = "Data source not found.",
+                                             Detail = "Data source could not be found with specified data source guid.",
+                                             Instance = "DA33EB64-13EF-46CC-B3E6-785E4027377A"
+                                         };
                 return NotFound(problem);
             }
 
@@ -113,22 +116,32 @@ namespace API.Controllers
                 if(project == null)
                 {
                     ProblemDetails problem = new ProblemDetails
-                    {
-                        Title = "Project could not be found.",
-                        Detail = "The project could not be found with the specified source Uri and data source guid",
-                        Instance = "993252E8-61C4-422D-A547-EB9F56BA47B7"
-                    };
+                                             {
+                                                 Title = "Project could not be found.",
+                                                 Detail =
+                                                     "The project could not be found with the specified source Uri and data source guid.",
+                                                 Instance = "993252E8-61C4-422D-A547-EB9F56BA47B7"
+                                             };
                     return NotFound(problem);
                 }
                 return Ok(mapper.Map<Project, WizardProjectResourceResult>(project));
             } catch(NotSupportedByExternalApiException e)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "External API does not support the functionality from the method.",
-                    Detail = e.Message,
-                    Instance = "DD815174-8711-4EF0-B01B-776709EDF485"
-                };
+                                         {
+                                             Title = "External API does not support the functionality from the method.",
+                                             Detail = e.Message,
+                                             Instance = "DD815174-8711-4EF0-B01B-776709EDF485"
+                                         };
+                return BadRequest(problem);
+            } catch(ExternalException e)
+            {
+                ProblemDetails problem = new ProblemDetails
+                                         {
+                                             Title = "An problem encountered while using the external API.",
+                                             Detail = e.Message,
+                                             Instance = "AA4FC30F-85F0-4120-A479-728DADABAB32"
+                                         };
                 return BadRequest(problem);
             }
         }
@@ -156,37 +169,47 @@ namespace API.Controllers
             if(!Guid.TryParse(dataSourceGuid, out Guid _))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Specified guid is not valid.",
-                    Detail = "The specified guid is not a real or valid guid.",
-                    Instance = "D84D3112-855D-480A-BCDE-7CADAC2C6C55"
-                };
+                                         {
+                                             Title = "Specified guid is not valid.",
+                                             Detail = "The specified guid is not a real or valid guid.",
+                                             Instance = "D84D3112-855D-480A-BCDE-7CADAC2C6C55"
+                                         };
                 return BadRequest(problem);
             }
 
             if(!dataProviderService.IsExistingDataSourceGuid(dataSourceGuid))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Data source not found.",
-                    Detail = "Data source could not be found with specified data source guid.",
-                    Instance = "4FB90F9A-8499-40F1-B7F3-3C2838BDB1D4"
-                };
+                                         {
+                                             Title = "Data source not found.",
+                                             Detail = "Data source could not be found with specified data source guid.",
+                                             Instance = "4FB90F9A-8499-40F1-B7F3-3C2838BDB1D4"
+                                         };
                 return NotFound(problem);
             }
 
             try
             {
-                IEnumerable<Project> projects = await dataProviderService.GetAllProjects(dataSourceGuid, token, needsAuth);
+                IEnumerable<Project> projects =
+                    await dataProviderService.GetAllProjects(dataSourceGuid, token, needsAuth);
                 return Ok(mapper.Map<IEnumerable<Project>, IEnumerable<WizardProjectResourceResult>>(projects));
             } catch(NotSupportedByExternalApiException e)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "External API does not support the functionality from the method.",
-                    Detail = e.Message,
-                    Instance = "8492B945-7C09-425B-9D1D-77869CE67146"
-                };
+                                         {
+                                             Title = "External API does not support the functionality from the method.",
+                                             Detail = e.Message,
+                                             Instance = "8492B945-7C09-425B-9D1D-77869CE67146"
+                                         };
+                return BadRequest(problem);
+            } catch(ExternalException e)
+            {
+                ProblemDetails problem = new ProblemDetails
+                                         {
+                                             Title = "An problem encountered while using the external API.",
+                                             Detail = e.Message,
+                                             Instance = "DE7A6BFD-2A72-46CC-AB6E-1D8568F2EB19"
+                                         };
                 return BadRequest(problem);
             }
         }
@@ -208,42 +231,43 @@ namespace API.Controllers
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetProjectByGuidFromExternalDataSource([FromQuery] string dataSourceGuid,
-                                                                    [FromQuery] string accessToken,
-                                                                    int projectId,
-                                                                    [FromQuery] bool needsAuth)
+            [FromQuery] string accessToken,
+            int projectId,
+            [FromQuery] bool needsAuth)
         {
             if(!Guid.TryParse(dataSourceGuid, out Guid _))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Specified guid is not valid.",
-                    Detail = "The specified guid is not a real or valid guid.",
-                    Instance = "019146D8-4162-43DD-8531-57DDD26E221C"
-                };
+                                         {
+                                             Title = "Specified guid is not valid.",
+                                             Detail = "The specified guid is not a real or valid guid.",
+                                             Instance = "019146D8-4162-43DD-8531-57DDD26E221C"
+                                         };
                 return BadRequest(problem);
             }
 
             if(!dataProviderService.IsExistingDataSourceGuid(dataSourceGuid))
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Data source not found.",
-                    Detail = "Data source could not be found with specified data source guid.",
-                    Instance = "4E3837F4-9D35-40C4-AB7C-D325FBA225E6"
-                };
+                                         {
+                                             Title = "Data source not found.",
+                                             Detail = "Data source could not be found with specified data source guid.",
+                                             Instance = "4E3837F4-9D35-40C4-AB7C-D325FBA225E6"
+                                         };
                 return NotFound(problem);
             }
 
             try
             {
-                Project project = await dataProviderService.GetProjectByGuid(dataSourceGuid, accessToken, projectId, needsAuth);
+                Project project =
+                    await dataProviderService.GetProjectByGuid(dataSourceGuid, accessToken, projectId, needsAuth);
 
                 if(project == null)
                 {
                     ProblemDetails problem = new ProblemDetails
                                              {
-                                                 Title = "Project not found",
-                                                 Detail = "Project could not be found with specified project guid",
+                                                 Title = "Project not found.",
+                                                 Detail = "Project could not be found with specified project guid.",
                                                  Instance = "0D96A77A-D35F-487C-B552-BF6D1C0CDD42"
                                              };
                     return NotFound(problem);
@@ -253,11 +277,20 @@ namespace API.Controllers
             } catch(NotSupportedByExternalApiException e)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "External API does not support the functionality from the method.",
-                    Detail = e.Message,
-                    Instance = "F20B0D1F-D6B7-4BCE-9BC8-28B9E9618214"
-                };
+                                         {
+                                             Title = "External API does not support the functionality from the method.",
+                                             Detail = e.Message,
+                                             Instance = "F20B0D1F-D6B7-4BCE-9BC8-28B9E9618214"
+                                         };
+                return BadRequest(problem);
+            } catch(ExternalException e)
+            {
+                ProblemDetails problem = new ProblemDetails
+                                         {
+                                             Title = "An problem encountered while using the external API.",
+                                             Detail = e.Message,
+                                             Instance = "21D8A923-02CB-4F1B-86C0-88FDA002294D"
+                                         };
                 return BadRequest(problem);
             }
         }
@@ -272,6 +305,7 @@ namespace API.Controllers
         /// <response code="400">The 400 Bad Request status code is returned when the specified data source guid is invalid.</response>
         /// <response code="404">The 404 Not Found status code is returned when no data source is found with the specified data source guid.</response>
         [HttpGet("oauth/callback/{provider}")]
+        [Authorize]
         [ProducesResponseType(typeof(Project), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
@@ -289,24 +323,25 @@ namespace API.Controllers
             if(dataSourceAdaptee == null)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "Data source not found.",
-                    Detail = "Data source could not be found with specified data source guid.",
-                    Instance = "5B4E11A6-8209-4F49-B76A-1EF4297D990F"
-                };
+                                         {
+                                             Title = "Data source not found.",
+                                             Detail = "Data source could not be found with specified data source guid.",
+                                             Instance = "5B4E11A6-8209-4F49-B76A-1EF4297D990F"
+                                         };
                 return NotFound(problem);
             }
 
-            IAuthorizedDataSourceAdaptee authorizedDataSourceAdaptee = dataSourceAdaptee as IAuthorizedDataSourceAdaptee;
+            IAuthorizedDataSourceAdaptee authorizedDataSourceAdaptee =
+                dataSourceAdaptee as IAuthorizedDataSourceAdaptee;
 
             if(authorizedDataSourceAdaptee == null)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "The specified provider does not allowed authorization",
-                    Detail = "The specified provider is not able to verify the code",
-                    Instance = "EB1F47B2-5526-41F3-8C69-8068F12A92D1"
-                };
+                                         {
+                                             Title = "The specified provider does not allowed authorization.",
+                                             Detail = "The specified provider is not able to verify the code.",
+                                             Instance = "EB1F47B2-5526-41F3-8C69-8068F12A92D1"
+                                         };
                 return BadRequest(problem);
             }
 
@@ -318,13 +353,24 @@ namespace API.Controllers
             } catch(NotSupportedByExternalApiException e)
             {
                 ProblemDetails problem = new ProblemDetails
-                {
-                    Title = "External API does not support the functionality from the method.",
-                    Detail = e.Message,
-                    Instance = "CDFFA448-38B1-450F-8D14-FB89FB7B5462"
-                };
+                                         {
+                                             Title = "External API does not support the functionality from the method.",
+                                             Detail = e.Message,
+                                             Instance = "CDFFA448-38B1-450F-8D14-FB89FB7B5462"
+                                         };
+                return BadRequest(problem);
+            } catch(ExternalException e)
+            {
+                ProblemDetails problem = new ProblemDetails
+                                         {
+                                             Title = "An problem encountered while using the external API.",
+                                             Detail = e.Message,
+                                             Instance = "7D445CB5-7C19-449C-B9FF-4214E4BE4CF0"
+                                         };
                 return BadRequest(problem);
             }
         }
+
     }
+
 }
