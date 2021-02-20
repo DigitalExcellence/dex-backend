e63d08636240fc0532792fa874bf08a825203c98
Niray <niray11@live.nl>
2021-02-20 10:15:57 +0100

    Added missing summaries and cleanup of jobscheduler

--- JobScheduler/ApiRequestHandler.cs
+++ JobScheduler/ApiRequestHandler.cs
@@ -1,22 +1,47 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+* 
+* This program is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation version 3 of the License.
+* 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Lesser General Public License for more details.
+* 
+* You can find a copy of the GNU Lesser General Public License 
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 using Models;
 using Newtonsoft.Json;
-using System.Net.Http;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace JobScheduler
 {
 
+    /// <summary>
+    ///     This is the interface of the API Request handler
+    /// </summary>
     public interface IApiRequestHandler
     {
+
         Task<List<UserTask>> GetExpectedGraduationUsersAsync();
 
         void SetGraduationTaskStatusToMailed(int userTaskId);
 
     }
 
+    /// <summary>
+    ///     This is the implementation of the API request handler.
+    /// </summary>
     public class ApiRequestHandler : IApiRequestHandler
     {
+
         /// <summary>
         ///     This is the HttpClient
         /// </summary>
@@ -33,16 +58,26 @@ namespace JobScheduler
             this.config = config;
         }
 
+        /// <summary>
+        ///     This is the method which requests the API for expecting graduating users
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<UserTask>> GetExpectedGraduationUsersAsync()
         {
-            HttpResponseMessage response = await client.GetAsync("api/UserTask/CreateUserTasks/" + config.JobSchedulerConfig.TimeRange);
+            HttpResponseMessage response =
+                await client.GetAsync("api/UserTask/CreateUserTasks/" + config.JobSchedulerConfig.TimeRange);
             return JsonConvert.DeserializeObject<List<UserTask>>(await response.Content.ReadAsStringAsync());
         }
 
+        /// <summary>
+        ///     This is the method which sets the user tasks to status 'mailed'.
+        /// </summary>
+        /// <param name="userTaskId"></param>
         public void SetGraduationTaskStatusToMailed(int userTaskId)
         {
             client.PutAsync("api/UserTask/SetToMailed/" + userTaskId, null);
         }
 
     }
+
 }
--- JobScheduler/GraduationWorker.cs
+++ JobScheduler/GraduationWorker.cs
@@ -1,33 +1,66 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+* 
+* This program is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation version 3 of the License.
+* 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Lesser General Public License for more details.
+* 
+* You can find a copy of the GNU Lesser General Public License 
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 using MessageBrokerPublisher.HelperClasses;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace JobScheduler
 {
+
+    /// <summary>
+    ///     This is the worker service which is responsible for running graduation related jobs.
+    /// </summary>
     public class GraduationWorker : BackgroundService
     {
+
+        private readonly Config config;
+        private readonly IEmailSender emailSender;
         private readonly ILogger<GraduationWorker> logger;
         private readonly IApiRequestHandler requestHandler;
-        private readonly IEmailSender emailSender;
-        private readonly Config config;
 
+        /// <summary>
+        ///     This is the constructor of the worker service
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="apiRequestHandler"></param>
+        /// <param name="emailSender"></param>
+        /// <param name="config"></param>
         public GraduationWorker(ILogger<GraduationWorker> logger,
                                 IApiRequestHandler apiRequestHandler,
                                 IEmailSender emailSender,
                                 Config config)
         {
-            
             this.logger = logger;
             requestHandler = apiRequestHandler;
             this.emailSender = emailSender;
             this.config = config;
         }
 
+        /// <summary>
+        ///     This is the asynchronous method which executes the job.
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             // Wait till API and Identity are started
@@ -42,6 +75,10 @@ namespace JobScheduler
             }
         }
 
+        /// <summary>
+        ///     This is the graduation job. It requests expecting graduating users and sends them an email to inform them.
+        /// </summary>
+        /// <returns></returns>
         private async Task GraduationJob()
         {
             try
@@ -52,7 +89,9 @@ namespace JobScheduler
                 {
                     foreach(UserTask userTask in userTasks)
                     {
-                        emailSender.Send(userTask.User.Email, "DeX: you are graduating, please convert your account.", null);
+                        emailSender.Send(userTask.User.Email,
+                                         "DeX: you are graduating, please convert your account.",
+                                         null);
                         requestHandler.SetGraduationTaskStatusToMailed(userTask.Id);
                     }
                 }
@@ -61,5 +100,7 @@ namespace JobScheduler
                 logger.LogCritical(e.InnerException + " " + e.Message);
             }
         }
+
     }
+
 }
