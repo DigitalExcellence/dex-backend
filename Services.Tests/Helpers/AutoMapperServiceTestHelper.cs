8bb27789ea66d99d26b710e41af81abf78ed274d
Ruben Fricke <rubenfricke@gmail.com>
2021-03-23 15:46:04 +0100

    Added documentation to the Automapper helper

--- Services.Tests/Helpers/AutoMapperServiceTestHelper.cs
+++ Services.Tests/Helpers/AutoMapperServiceTestHelper.cs
@@ -1,3 +1,20 @@
+/*
+* Digital Excellence Copyright (C) 2020 Brend Smits
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* You can find a copy of the GNU Lesser General Public License
+* along with this program, in the LICENSE.md file in the root project directory.
+* If not, see https://www.gnu.org/licenses/lgpl-3.0.txt
+*/
+
 using AutoMapper;
 using Models;
 using Services.ExternalDataProviders;
@@ -6,9 +23,16 @@ using Services.ExternalDataProviders.Resources;
 namespace Services.Tests.Helpers
 {
 
+    /// <summary>
+    ///     This class is a helper class for creating the IMapper interface in the service test project.
+    /// </summary>
     public static class AutoMapperServiceTestHelper
     {
 
+        /// <summary>
+        ///     This method is responsible for creating and returning the IMapper interface.
+        /// </summary>
+        /// <returns>This method returns the IMapper interface.</returns>
         public static IMapper GetIMapper()
         {
             MapperConfiguration config = new MapperConfiguration(conf =>
