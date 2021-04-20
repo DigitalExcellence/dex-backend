9338754755fb83cab4fc7c3d4730808dc0e83a26
Joep Verhoeven <joep.verhoeven01@gmail.com>
2021-04-20 09:53:26 +0200

    Added Docker Swarm test controller

---
+++ API/Controllers/DockerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+    /// <summary>
+    /// A api controller for testing DockerSwarm Scaling
+    /// </summary>
+    [ApiController]
+    public class DockerController : Controller
+    {
+
+        /// <summary>
+        /// Simulates endpoint
+        /// </summary>
+        /// <returns>Pong</returns>
+        [HttpGet("/api/docker/ping")]
+        public async Task<IActionResult> Ping()
+        {
+            await Task.Delay(new Random().Next(10,30));
+            return Ok("Pong");
+        }
+
+    }
+
+}
