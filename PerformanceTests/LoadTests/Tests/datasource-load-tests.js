34ebe74932643238baf6f08647a31a43ac3dc0e2
Joep Verhoeven <joep.verhoeven01@gmail.com>
2021-06-02 10:49:53 +0200

    Changed metrics

--- PerformanceTests/LoadTests/Tests/datasource-load-tests.js
+++ PerformanceTests/LoadTests/Tests/datasource-load-tests.js
@@ -1,36 +1,5 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { Counter, Rate, Trend } from 'k6/metrics';
 
-const DEFAULT_SLEEP_DURATION = 0.1; // Sleep duration in seconds.
-const MAX_REQUEST_DURATION = 500; // in ms.
-const MAX_FAIL_RATE = 0.01; // Max failrate should not be higher than 1%.
-
-export let GetDataSourceDuration = new Trend('Get datasource Duration');
-export let GetDataSourceFailRate = new Rate('Get datasource Fail Rate')
-export let GetDataSourceReqs = new Counter('Get datasource Requests');
-
-export let options = {
-    thresholds: {
-        'Get datasource Duration': [`p(99)<${MAX_REQUEST_DURATION}`], // 99% of requests must complete below the max duration.
-        'Get datasource Fail Rate': [`rate<${MAX_FAIL_RATE}`] // Fail rate may not be more as the max failrate.
-    }
-};
-
-export default function () {
-    let res = http.get(`${__ENV.BASE_ADDRESS}/Datasource`, {
-        tags: { name: 'GetDatasources' },
-    });
-
-    // Count the total amount of requests send to this endpoint.
-    GetDataSourceReqs.add(1);
-    // Add the duration of this request.
-    GetDataSourceDuration.add(res.timings.duration);
-    // Validate result
-    const result = check(res, {
-        'Get Datasources status is 200': (r) => r.status = 200,
-    });
-    GetDataSourceFailRate.add(!result);
-
-    sleep(DEFAULT_SLEEP_DURATION);
+export default function() {
+    http.get(`${__ENV.BASE_ADDRESS}/Datasource`);
 }
--- PerformanceTests/LoadTests/load-tests.js
+++ PerformanceTests/LoadTests/load-tests.js
@@ -1,44 +1,41 @@
 import projectTest from './Tests/project-load-tests.js';
-import { options as projectTestOptions } from './Tests/project-load-tests.js';
 import dataSourceTest from './Tests/project-load-tests.js';
-import { options as dataSourceTestOptions } from './Tests/datasource-load-tests.js';
 
 
-// Get the thresholds of each test
-let thresholdOptionsSum = {} // here we use the imported options to create a sum op options to run all tests.
-Object.entries(dataSourceTestOptions.thresholds).forEach(item =>
-    thresholdOptionsSum[item[0]] = item[1]);
-// Object.entries(projectTestOptions.thresholds).forEach(item =>
-//     thresholdOptionsSum[item[0]] = item[1]);
-
-
+const defaultScenarioRuntime = 30;
+const virtualUsersPerScenario = 100;
 
 export let options = {
     // Test stages
     scenarios: {
         getDataSourceScenario: {
-            executor: "ramping-vus",
-            stages: [
-                { duration: '10s', target: 50 }, // simulate ramp-up of traffic from 60 to 200 users over 320 seconds.
-                { duration: '30s', target: 50 }, // stay at 100 users for 200 seconds
-                { duration: '10s', target: 0 }, // ramp-down to 0 users
-            ],
+            executor: "constant-vus",
+            vus: virtualUsersPerScenario,
+            gracefulStop: '0s',
+            tags: { test_type: 'getDataSourceScenario' },
+            duration: defaultScenarioRuntime + 's',
+            startTime: defaultScenarioRuntime * 0 + 's',
             // The function the scenario will execute.
             exec: "testDatasourceEndpoints"
         },
         getProjectsScenario: {
             // The executor the scenario will use see : https://k6.io/docs/using-k6/scenarios/executors/
-            executor: "ramping-vus",
-            stages: [
-                { duration: '10s', target: 50 }, // simulate ramp-up of traffic from 60 to 200 users over 320 seconds.
-                { duration: '30s', target: 50 }, // stay at 100 users for 200 seconds
-                { duration: '10s', target: 0 }, // ramp-down to 0 users
-            ],
+            executor: "constant-vus",
+            vus: virtualUsersPerScenario,
+            gracefulStop: '0s',
+            tags: { test_type: 'getProjectsScenario' },
+            duration: defaultScenarioRuntime + 's',
+            startTime: defaultScenarioRuntime * 1 + 's',
             // The function the scenario will execute.
             exec: "testProjectEndpoints"
-        },   
+        },
     },
-    //thresholds: thresholdOptionsSum
+    thresholds: {
+        'http_req_duration{test_type:getDataSourceScenario}': ['p(95)<250', 'p(99)<350'],
+        'http_req_failed{test_type:getDataSourceScenario}': ['rate<0.01'],
+        'http_req_duration{test_type:getProjectsScenario}': ['p(95)<250', 'p(99)<350'],
+        'http_req_failed{test_type:getProjectsScenario}': ['rate<0.01'],
+    }
 }
 
 export function testProjectEndpoints() {
