a68821b05ae5228aaac922911195e08346f852ab
Nicole Bours(Desktop) <nora1410xd@users.noreply.github.com>
2021-06-16 14:05:34 +0200

    Create UserProjectCommentLikeResourceResult.cs

---
+++ API/Resources/UserProjectCommentLikeResourceResult.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Resources
+{/// <summary>
+/// 
+/// </summary>
+    public class UserProjectCommentLikeResourceResult
+    {
+        public int Id { get; set; }
+        public string comment { get; set; }
+
+    }
+}
