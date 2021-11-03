417f5f589da70780b2090ccc063d443851936eb0
Mees van Straten <41986758+MeesvanStraten@users.noreply.github.com>
2021-11-03 11:18:20 +0100

    Apply suggestions from code review

--- Data/Configurations/ProjectConfiguration.cs
+++ Data/Configurations/ProjectConfiguration.cs
@@ -30,7 +30,7 @@ namespace Data.Configurations
             builder.HasKey(p => p.Id);
 
             builder.Property(p => p.Name).IsRequired();
-            builder.Property(p => p.Description).IsRequired();
+            
             builder.Property(p => p.ShortDescription).IsRequired();
             builder.Property(p => p.Created).IsRequired();
             builder.Property(p => p.Updated).IsRequired();
