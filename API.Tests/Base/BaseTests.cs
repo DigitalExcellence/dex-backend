1b045f9faa388afea92fd257de59997368d0c811
Sven <svenroermond@hotmail.com>
2021-11-19 12:36:00 +0100

    Update BaseTests.cs

--- API.Tests/Base/BaseTests.cs
+++ API.Tests/Base/BaseTests.cs
@@ -77,13 +77,14 @@ namespace API.Tests.Base
                     Address = identityAddress + "connect/token",
                     GrantType = IdentityModel.OidcConstants.GrantTypes.ClientCredentials,
                     ClientId = "Test",
-                    ClientSecret = "Test",
-                    Parameters =
-                                {
-                                    { "username", "bob"},
-                                    { "password", "bob"},
-                                    { "scope", "ProjectWrite"}
-                                }
+                    ClientSecret = "Test"
+                    //,
+                    //Parameters =
+                    //            {
+                    //                { "username", "bob"},
+                    //                { "password", "bob"},
+                    //                { "scope", "ProjectWrite"}
+                    //            }
                 });
 
                 return response.AccessToken;
