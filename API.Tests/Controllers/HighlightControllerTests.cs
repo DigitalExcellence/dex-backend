ca0c2df26a404ce91da042bced2d53bd8c9ea6db
Mees van Straten <41986758+MeesvanStraten@users.noreply.github.com>
2021-11-22 09:33:57 +0100

    Update HighlightControllerTests.cs

--- API.Tests/Controllers/HighlightControllerTests.cs
+++ API.Tests/Controllers/HighlightControllerTests.cs
@@ -9,29 +9,35 @@ using API.Tests.Enums;
 using API.Tests.Helpers;
 using Newtonsoft.Json;
 using Models;
+using API.Resources;
 
 namespace API.Tests.Controllers
 {
     public class HighlightControllerTests : BaseTests
     {
-        //[Theory]
-        //[InlineData(UserRole.RegisteredUser, HttpStatusCode.Forbidden)]
-        //[InlineData(UserRole.Admin, HttpStatusCode.Created)]
-        //[InlineData(UserRole.DataOfficer, HttpStatusCode.Forbidden)]
-        //[InlineData(UserRole.PrUser, HttpStatusCode.Created)]
-        //[InlineData(UserRole.Alumni, HttpStatusCode.Forbidden)]
-        //public async Task CreateHighlight_Returns_Expected_Result_For_All_Roles(UserRole role, HttpStatusCode expectedResult)
-        //{
-        //    // Arrange
-        //    await AuthenticateAs(role);
-        //    Project project = SeedUtility.RandomProject();
-        //    await TestClient.PostAsJsonAsync("project", project);
+        [Theory]
+        [InlineData(UserRole.RegisteredUser, HttpStatusCode.Forbidden)]
+        [InlineData(UserRole.Admin, HttpStatusCode.Created)]
+        [InlineData(UserRole.DataOfficer, HttpStatusCode.Forbidden)]
+        [InlineData(UserRole.PrUser, HttpStatusCode.Created)]
+        [InlineData(UserRole.Alumni, HttpStatusCode.Forbidden)]
+        public async Task CreateHighlight_Returns_Expected_Result_For_All_Roles(UserRole role, HttpStatusCode expectedResult)
+        {
+            // Arrange
+            await AuthenticateAs(role);
+            Project project = SeedUtility.RandomProject();
+            HttpResponseMessage postProjectResponse = await TestClient.PostAsJsonAsync("project", project);
 
-        //    // Act
-        //    HttpResponseMessage response = await TestClient.PostAsJsonAsync("highlight", project);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(expectedResult);
-        //}
+            string responseContent = await postProjectResponse.Content.ReadAsStringAsync();
+            Project projectToHighlight = JsonConvert.DeserializeObject<Project>(responseContent);
+
+            // Act
+            HighlightInput highlightInput = new HighlightInput{ ProjectId = projectToHighlight.Id, Description = projectToHighlight.Description };
+            HttpResponseMessage response = await TestClient.PostAsJsonAsync("highlight", highlightInput);
+
+            // Assert
+            response.StatusCode.Should().Be(expectedResult);
+        }
     }
 }
