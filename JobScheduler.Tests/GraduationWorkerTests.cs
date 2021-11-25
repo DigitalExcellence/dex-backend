6e264e252ee13a16b23646076c676f160df825fb
Sven Hansen <76601644+1SvenHansen@users.noreply.github.com>
2021-11-25 14:04:05 +0100

    Creating tests

--- JobScheduler.Tests/GraduationWorkerTests.cs
+++ JobScheduler.Tests/GraduationWorkerTests.cs
@@ -5,6 +5,12 @@ using NUnit.Framework;
 using JobScheduler;
 using MessageBrokerPublisher.HelperClasses;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace JobScheduler.Tests
 {
@@ -13,15 +19,33 @@ namespace JobScheduler.Tests
     {
 
         [Test]
-        public void GraduationJob_EmailNotification_NotificationGraduating()
+        public async Task GraduationJob_EmailNotification_NotificationGraduating()
         {
-            IEmailSender emailSender = new EmailSender();
-            ILogger logger;
-            IApiRequestHandler apiRequestHandler = new ApiRequestHandler();
+            // Arrange
             Config config = new Config();
 
-            GraduationWorker gw = new GraduationWorker(logger, apiRequestHandler, emailSender, config);
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(Mock.Of<IEmailSender>());
+            services.AddSingleton(Mock.Of<IApiRequestHandler>());
+            services.AddHostedService<GraduationWorker>();
 
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            Mock<IEmailSender> emailSenderMock = Mock.Get(serviceProvider.GetService<IEmailSender>());
+            GraduationWorker backgroundService = serviceProvider.GetService<IHostedService>() as GraduationWorker;
+
+            // Act
+            await backgroundService?.StartAsync(CancellationToken.None);
+            await Task.Delay(1000);
+
+            // Assert
+            emailSenderMock.Verify(c => c.Send(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+                ), Times.Once);
+
+            await backgroundService?.StopAsync(CancellationToken.None);
         }
     }
 }
