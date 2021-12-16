31cb5996afd3caf4245db0850d3124de48e297f7
Bouman,Dave D.H.M <d.reijnen@student.fontys.nl>
2021-12-16 13:22:23 +0100

    Added cascade

---
+++ Data/Migrations/20211216122156_cascadeondelete.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _4_Data.Migrations
+{
+    public partial class cascadeondelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Project_ProjectId",
+                table: "ProjectTag");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Tag_TagId",
+                table: "ProjectTag");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Project_ProjectId",
+                table: "ProjectTag",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Tag_TagId",
+                table: "ProjectTag",
+                column: "TagId",
+                principalTable: "Tag",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Project_ProjectId",
+                table: "ProjectTag");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTag_Tag_TagId",
+                table: "ProjectTag");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Project_ProjectId",
+                table: "ProjectTag",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTag_Tag_TagId",
+                table: "ProjectTag",
+                column: "TagId",
+                principalTable: "Tag",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
